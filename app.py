8feba70084e9514c37a6505a482e78669728bcbd
Guilherme Denardi <gdenardi@outlook.com>
2024-04-22 12:46:15 -0300

    Creating the first test route

--- app.py
+++ app.py
@@ -2,5 +2,9 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route("/hello-world", methods=["GET"])
+def hello_world():
+    return "Hello World"
+
 if __name__ == "__main__":
     app.run(debug=True)
